9f5e66b8165b1a9133c796a7e77521913bf5e61f
chai2543 <52267052+chai2543@users.noreply.github.com>
2019-07-05 16:36:14 +0700

    Add files via upload

---
+++ Total.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[48]:
+
+
+print("--------------------------------")
+print("Chai shop")
+print("--------------------------------")
+print("water" ,3 , 10 ,"THB" ,3*10,"THB")
+print("Ole" ,10,10, "THB" ,10*10, "THB")
+print("Total" ,30+100, "THB")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ chai.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+print("----------------------------------------")
+print("chai Shop")
+print("----------------------------------------")
+print("Water      x 1   :10 THB")
+print("milk         x 1   : 15  THB")
+print("Party       x 1    :20 THB")
+print("----------------------------------------")
+print("Total                :45 THB")
+
+
+# In[19]:
+
+
+print(type(10))
+print(type("Hello"))    
+print(type(True)) 
+print(type(10.3))
+print(type("1j"))   
+      
+
+
+# In[20]:
+
+
+print(10+3.5)
+
+
+# In[ ]:
+
+
+
+
