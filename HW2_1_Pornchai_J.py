20fc4529e914addf11426bd8df93fe3b6e6a52a8
chai2543 <52267052+chai2543@users.noreply.github.com>
2019-07-22 17:57:31 +0700

    HW2

---
+++ HW2_1_Pornchai_J.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#นาย พรชัย  จันมิตร 362515241011 EE36241N
+print("--------------Menu--------------")
+print("Welcome to Icecream Shop")
+print("1. Strawberry 80 THB")
+print("2. Vanila 50 THB")
+print("3. Chocolate 40 THB")
+print("4. Lemon 90 THB")
+print("5. Milk 10 THB")
+i=int(input("What do you want?"))
+if i==1 :
+    print("You order : 1 Strawberry 80 THB")
+elif i==2 :
+    print("You order : 2 Vanila 50 THB")
+elif i==3 :
+    print("You order : 3 Chocolate 40 THB")
+elif i==4 :
+    print("You order : 4 Lemon 90 THB")
+elif i==5 :
+    print("You order : 5 Milk 10 THB")
+
+
+# In[ ]:
+
+
+
+
